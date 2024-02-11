0386744976d73a4d0a1017d00ffb3b31283d72a4
samwhelp <samwhelp@users.noreply.github.com>
2024-02-11 09:11:53 +0000

    fix

--- prototype/main/lxqt-config/Main/asset/overlay/etc/skel/.config/openbox/helper/share/gen/openbox-gen-rc/Section/Keybind/Screenshot.php
+++ prototype/main/lxqt-config/Main/asset/overlay/etc/skel/.config/openbox/helper/share/gen/openbox-gen-rc/Section/Keybind/Screenshot.php
@@ -1,18 +1,33 @@
 
 
-	<!-- Take a screenshot of the current window with scrot when Alt+Print are pressed -->
-	<keybind key="A-Print">
+	<keybind key="Print">
 		<action name="Execute">
-			<command>scrot -s</command>
+			<command>screengrab</command>
 		</action>
 	</keybind>
 
+
+
+
 	<!-- Launch scrot when Print is pressed -->
+	<!--
 	<keybind key="Print">
 		<action name="Execute">
 			<command>scrot</command>
 		</action>
 	</keybind>
+	//-->
+
+
+	<!-- Take a screenshot of the current window with scrot when Alt+Print are pressed -->
+	<!--
+	<keybind key="A-Print">
+		<action name="Execute">
+			<command>scrot -s</command>
+		</action>
+	</keybind>
+	//-->
+
 
 <?php
 /*
