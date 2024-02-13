c16ef7f09ed112e7e46dddc48db814ba593961bd
samwhelp <samwhelp@users.noreply.github.com>
2024-02-13 22:09:19 +0000

    fix

---
+++ sample/compiz/scripts/package-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+sudo apt-get install \
+	compiz \
+	compiz-plugins \
+	compiz-plugins-default \
+	compiz-plugins-main \
+	compiz-plugins-extra \
+	compizconfig-settings-manager
