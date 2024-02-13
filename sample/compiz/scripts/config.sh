ddcebe25258278ed8f8321c863b77a9a55689607
samwhelp <samwhelp@users.noreply.github.com>
2024-02-13 22:00:36 +0000

    fix

---
+++ sample/compiz/scripts/config.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+gsettings set org.gnome.desktop.wm.preferences theme 'Numix'
+
+#gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:close'
+
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
