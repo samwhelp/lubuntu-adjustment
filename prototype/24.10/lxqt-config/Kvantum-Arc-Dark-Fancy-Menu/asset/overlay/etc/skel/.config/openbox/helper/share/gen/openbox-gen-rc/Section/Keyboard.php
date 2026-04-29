153c52e1c929b427ab5d4915631da579c678fbaa
samwhelp <samwhelp@users.noreply.github.com>
2026-04-29 15:48:06 +0800

    fix

--- prototype/24.10/lxqt-config/Kvantum-Arc-Dark-Fancy-Menu/asset/overlay/etc/skel/.config/openbox/helper/share/gen/openbox-gen-rc/Section/Keyboard.php
+++ prototype/24.10/lxqt-config/Kvantum-Arc-Dark-Fancy-Menu/asset/overlay/etc/skel/.config/openbox/helper/share/gen/openbox-gen-rc/Section/Keyboard.php
@@ -31,7 +31,7 @@
 <?php include_once(__DIR__ . '/Keybind/WindowBeginMove.php'); ?>
 <?php include_once(__DIR__ . '/Keybind/WindowBeginResize.php'); ?>
 
-<?php include_once(__DIR__ . '/Keybind/WindowTiling.php'); ?>
+<?php //include_once(__DIR__ . '/Keybind/WindowTiling.php'); ?>
 <?php include_once(__DIR__ . '/Keybind/WindowTilingMoveSide.php'); ?>
 <?php include_once(__DIR__ . '/Keybind/WindowTilingMoveCorner.php'); ?>
 <?php include_once(__DIR__ . '/Keybind/WindowMoveToCenter.php'); ?>
